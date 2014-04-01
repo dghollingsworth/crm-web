5fb9f0db5a24d13964f53ab4e719710ed94edc25
dghollingsworth <dghollingsworth@gmail.com>
2014-04-01 16:05:38 -0400

    finished step 3: adding new routes

--- crm.rb
+++ crm.rb
@@ -1,6 +1,29 @@
 require 'sinatra'
 
+get '/contacts' do 
+	@contacts_title = "Contacts"
+	erb :contacts
+end
+
+get '/contacts/new' do
+	 
+end
+
+get '/contacts/:id' do 
+	# so it doesn't literally match contacts/:id
+	# it actually matches something like: contacts/poop, contacts/3, etc
+
+
+	# how do i access the value of :id?
+	"hello #{params[:name]}!"
+end
+
+get '/contacts/:id/edit' do 
+	# /contacts/SOMETHING/edit
+end
+
 get '/' do 
 	@crm_app_name = "My CRM"
+	@title_name = "Main Menu"
 	erb :index
 end
